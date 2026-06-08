6de80df995a39171269632f6729cfa24fffa514c
rustamovuz <nodirbekrustamov771@gmail.com>
2026-06-08 16:02:38 +0500

    updated

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const connectDB = require("./config/db.config")
-const authorRouter = require("./router/author.routes")
-const bookRouter = require("./router/book.routes")
-const errorMiddleware = require("./middleware/error.middleware")
-const authRouter = require("./router/auth.routes")
-const quoteRouter = require("./router/quote.routes")
-require("dotenv").config()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db.config");
+const authorRouter = require("./router/author.routes");
+const bookRouter = require("./router/book.routes");
+const errorMiddleware = require("./middleware/error.middleware");
+const cookieParser = require("cookie-parser");
+const CitationRouter = require("./router/citation.routes");
 
-const app = express()
-const PORT = process.env.PORT || 3000
-app.use(express.json())
-app.use(cors())
+const app = express();
 
-connectDB()
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cookieParser());
+connectDB();
 
-// Router
 app.use(authorRouter);
 app.use(bookRouter);
-app.use(authRouter);
-app.use(quoteRouter);
-
+app.use(CitationRouter);
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log("Server is running at: " + PORT);
-})
+});
